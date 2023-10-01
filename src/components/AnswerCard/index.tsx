be80bf34a954f3832fa80050b39ec7199f79ec9f
Zeyad Omran <ziomran@gmail.com>
2023-10-01 00:09:39 -0600

    add pics

--- src/components/AnswerCard/index.tsx
+++ src/components/AnswerCard/index.tsx
@@ -8,10 +8,10 @@ interface Props {
 const AnswerCard: FC<Props> = ({ src, onClick }) => {
 	return (
 		<div
-			className="bg-purple-900 rounded-sm p-2 w-48 h-48 cursor-pointer hover:bg-purple-400 transition-all duration-500 ease-in-out"
+			className="bg-purple-900 rounded-sm p-2 w-64 h-64 cursor-pointer hover:bg-purple-400 transition-all duration-500 ease-in-out overflow-hidden"
 			onClick={() => onClick()}
 		>
-			<img src={src} className="rounded-sm" />
+			<img src={src} className="rounded-sm w-full h-full object-center" />
 		</div>
 	);
 };
